65f0bdd9ca9b94087c81fc63214972dce96cd600
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-12-09 01:51:00 +0800

    Update build.gradle.kts

--- kaidl/build.gradle.kts
+++ kaidl/build.gradle.kts
@@ -14,24 +14,15 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-gradlePlugin {
-    plugins {
-        create("golang") {
-            id = "com.github.kr328.kaidl"
-            implementationClass = "com.github.kr328.kaidl"
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/emp3826/kaidl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("ACCESS_TOKEN")
+            }
         }
     }
-}
-
-publishing {
-  repositories {
-    maven {
-      name = "GitHubPackages"
-      url = uri("https://maven.pkg.github.com/emp3826/kaidl")
-      credentials {
-        username = System.getenv("GITHUB_ACTOR")
-        password = System.getenv("ACCESS_TOKEN")
-      }
-    }
-  }
 }
